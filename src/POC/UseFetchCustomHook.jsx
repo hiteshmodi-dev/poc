638e8f20670da4f4f190c6dbaa695e7d77a50766
Hiteshkumar Modi <hiteshmodi@ibm.com>
2025-04-15 13:06:18 +0530

    file update

--- src/POC/UseFetchCustomHook.jsx
+++ src/POC/UseFetchCustomHook.jsx
@@ -2,39 +2,36 @@ import { useState, useEffect } from 'react';
 
 const useFetch = (url) => {
   const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
-      setIsLoading(true);
       try {
-        const response = await fetch(url);
-        const json = await response.json();
-        setData(json);
-      } catch (error) {
-        console.log(error);
-      } finally {
+        const cachedData = localStorage.getItem(url);
+        if (cachedData) {
+          setData(JSON.parse(cachedData));
+          setIsLoading(false);
+        } else {
+          const response = await fetch(url);
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          const data = await response.json();
+          localStorage.setItem(url, JSON.stringify(data));
+          setData(data);
+          setIsLoading(false);
+        }
+      } catch (err) {
+        setError(err);
         setIsLoading(false);
       }
     };
 
-    // fetchData();
-    const cacheData = localStorage.getItem(url);
-    if (cacheData) {
-      setData(JSON.parse(cacheData));
-      setIsLoading(false);
-    } else {
-      fetchData();
-    }
+    fetchData();
   }, [url]);
 
-  useEffect(() => {
-    if (data) {
-      localStorage.setItem(url, JSON.stringify(data));
-    }
-  }, [data, url]);
-
-  return [data, isLoading];
+  return [data, error, isLoading];
 };
 
 export default useFetch;
